289ac3f4d0fb950fa473f1262466ffe2b80e8689
“Julia <juliakahn1@gmail.com>
2023-08-07 15:30:24 -0700

    Completed #initialize #children methods

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -2,6 +2,8 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
 
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -9,7 +11,7 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    
+
   end
 
   def winning_node?(evaluator)
@@ -19,13 +21,20 @@ class TicTacToeNode
   # the current move.
   def children
     possible_board_states = []
-    open_pos = @board.open_positions
-    open_pos.each do |pos|
-      dup_board = @board.dup
-      dup_board[pos] = @next_mover_mark
-      possible_board_states << TicTacToeNode(dup_board, dup_board.next_mark, pos)
+    # open_pos = @board.open_positions # empty pos
+
+    # open_pos.each do |pos|
+    (0..2).each do |row|
+      (0..2).each do |col|
+        pos = [row, col]
+        if @board.empty?(pos)
+          dup_board = @board.dup
+          dup_board[pos] = @next_mover_mark
+          possible_board_states << TicTacToeNode.new(dup_board, dup_board.next_mark, pos)
+        end
+      end
     end
 
-    return possible_board_states
+    possible_board_states
   end
 end
